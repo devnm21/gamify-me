6ce1ad1842ea75f61cddef02f33cb850912ae59e
Shrithan <shrithanofficial@gmail.com>
2022-08-27 20:02:37 +0530

    Feat: Added Login & SignUp Pages  :sparkles:

---
+++ frontend/pages/home/index.js
@@ -0,0 +1,11 @@
+import Nav from "../src/components/Navbar";
+import Hero from "../src/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  );
+}
